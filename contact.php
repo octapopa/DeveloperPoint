7bfa50cd1eb615b68136faf1c544929177b91e20
octapopa <octapopa@yahoo.com>
2013-06-05 12:49:19 +0300

    contact modification

--- contact.php
+++ contact.php
@@ -3,6 +3,13 @@
 	<title>Contact page</title>
 </head>
 <body>
-
+	<div class="conteiner">
+		<div class="head">
+			<img src="">
+		</div>
+		<div class="main-content">
+		</div>
+		<div class="footer"></div>
+	</div>
 </body>
 </html>
